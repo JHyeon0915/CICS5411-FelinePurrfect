a8f3e531c73f5c6b97f09b70db26e9001a50949f
Jeonghyeon Lee <dlqjdgus99@naver.com>
2025-10-31 01:30:43 -0300

    feat: bottom navigation tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,61 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+
+import { Colors } from '@/constants/colors';
+import {
+  Entypo as HomeIcon,
+  Ionicons as LogsIcon,
+  FontAwesome6 as MyCatsIcon,
+  Ionicons as SearchIcon,
+} from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary.DEFAULT,
+        tabBarInactiveTintColor: Colors.gray.DEFAULT,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <HomeIcon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="logs"
+        options={{
+          title: 'Logs',
+          tabBarLabel: 'Logs',
+          tabBarIcon: ({ color, size }) => (
+            <LogsIcon name="analytics-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <SearchIcon name="search-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mycats"
+        options={{
+          title: 'My Cats',
+          tabBarLabel: 'My Cats',
+          tabBarIcon: ({ color, size }) => (
+            <MyCatsIcon name="cat" size={size-3} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ constants/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: {
+    DEFAULT: '#155C8D',
+  },
+  gray: {
+    light: '#B7C2CB',
+    DEFAULT: '#99A6B0',
+    dark: '#5B6470',
+  }
+};
